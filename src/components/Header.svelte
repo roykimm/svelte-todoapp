7ba6f965a9320f2c03f3388a7a3f79081866bed1
roykimm <roykimmyunghoon@gmail.com>
2021-11-09 21:39:13 +0900

    t

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,7 @@
 <script>
     import { items } from "../stores/stores";
+    import { fly } from "svelte/transition";
+    let logo = "bellalogo.png";
 
     console.log($items);
     $: itemCount = $items.length;
@@ -11,6 +13,14 @@
 <div
     class="flex justify-between align-center px-5 py-5 text-gray-500 font-bold text-xl bg-gray-400"
 >
-    <span>Bella's Todolist</span>
+    <div class="flex">
+        <img
+            class="w-20 mx-5"
+            in:fly={{ delay: 1000, x: 100 }}
+            src={logo}
+            alt="bella!"
+        />
+        <span>Bella's Todolist</span>
+    </div>
     <span class="text-md">remains : {completedItemCount} / {itemCount}</span>
 </div>
